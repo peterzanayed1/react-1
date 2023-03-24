77b565102a438122a9907f1fefe1e14c66716ad7
unknown <peterzanayed1@gmail.com>
2023-03-24 17:40:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { toDoList } from "./toDoList"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <toDoList />
+    <button>add</button>
+    <button>Clear</button>
+    <div>Left to do: 0</div>
+
+    </>
+  
+  
   );
 }
 
---
+++ src/dataloop.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const dataloop = () => {
+  return (
+    <div>dataloop</div>
+  )
+}
